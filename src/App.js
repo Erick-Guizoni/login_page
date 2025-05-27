5d04a619f4e0b4592b62a78efc7f3ae6c99f192d
Erick Guizoni <164954594+Erick-Guizoni@users.noreply.github.com>
2025-05-26 21:50:45 -0300

    blabla

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import Input from './components/ui/input/Input';
+import Text from './components/ui/text/Text';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Text id="idLogin" text="LOGIN"/>
+        <Input/>
+        <Input/>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ui/text/Text.js
@@ -0,0 +1,8 @@
+import styles from "./text.module.scss"
+
+function Text({}){
+    return<>
+</>
+}
+
+export default Text;
