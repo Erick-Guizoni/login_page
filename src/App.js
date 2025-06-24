39809b65e7f9bb58fb87e7a81b9538b022325bfb
Erick Guizoni <164954594+Erick-Guizoni@users.noreply.github.com>
2025-06-23 21:56:00 -0300

    ss

--- src/App.js
+++ src/App.js
@@ -4,17 +4,25 @@ import Header from './components/container/header/Header';
 import LoginPage from './pages/login_page/LoginPage';
 import Home from './pages/home_page/Home'
 import FormCardPage from './pages/form_card/FormCardPage';
+import PrivateRoute from './PrivateRoute';
+import { AuthProvider } from './context/AuthContext';
 
 function App() {
   return (
+    <AuthProvider>
     <div className="App">
       <Header/>
       <Routes>
       <Route path='/' element={<LoginPage/>}/>
-      <Route path='/home' element={<Home/>}/>
+      <Route path='/home' element={
+        <PrivateRoute> 
+        <Home/>
+        </PrivateRoute> 
+      }/>
       <Route path='/formCard' element={<FormCardPage />} />
       </Routes>
     </div>
+    </AuthProvider>
   );
 }
 
